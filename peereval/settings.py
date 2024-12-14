1b6ea06c003d0c790ad9d353f10f60ccf37c71f9
shetkarneeraj <94823623+shetkarneeraj@users.noreply.github.com>
2024-12-14 15:41:42 +0530

    Minor changes

--- peereval/settings.py
+++ peereval/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'peereval.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),  # Cast to string
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'iitrpr',          # Database name
+        'USER': 'postgres',              # Your PostgreSQL username
+        'PASSWORD': '12345678',      # Your PostgreSQL password
+        'HOST': '127.0.0.1',           # Localhost
+        'PORT': '5432',                # Default PostgreSQL port
     }
 }
 
